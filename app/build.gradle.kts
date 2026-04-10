441a3ba428475b81210535f5dbc9df94e9f356da
dilipdas8370902534-sys <dilipdas8370902534@gmail.com>
2026-04-10 14:15:30 +0530

    Add Korean text recognition dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("com.google.mlkit:text-recognition-chinese:16.0.0")
     implementation("com.google.mlkit:text-recognition-devanagari:16.0.0")
     implementation("com.google.mlkit:text-recognition-japanese:16.0.0")
+    implementation("com.google.mlkit:text-recognition-korean:16.0.0") // এই লাইনটিই মিসিং ছিল!
 
     // ইন্টারনেট এবং API কলের জন্য OkHttp
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
